4715d1e123cd60f083e762ea7671c2fb2fa48ec6
esomly19 <esomly19@github.com>
2019-11-05 15:26:17 +0100

    changement dossier

---
+++ MediaNet-Staff/src/js/accueil.js
@@ -0,0 +1,26 @@
+
+document.getElementById("1").addEventListener("click", event => {
+    let url =   document.location.href;
+    var the_arr = url.substring(0, url.lastIndexOf('/'));
+    url=  the_arr+'/choisir';
+  window.location=url;
+  
+  });
+
+  
+document.getElementById("2").addEventListener("click", event => {
+    let url =   document.location.href;
+    var the_arr = url.substring(0, url.lastIndexOf('/'));
+    url=  the_arr+'/choisir';
+  window.location=url;
+  
+  });
+
+  
+document.getElementById("3").addEventListener("click", event => {
+    let url =   document.location.href;
+    var the_arr = url.substring(0, url.lastIndexOf('/'));
+    url=  the_arr+'/choisir';
+  window.location=url;
+  
+  });
