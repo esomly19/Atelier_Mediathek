bd925603a4835c67daf2dca4ff30a0ed9727b070
esomly19 <esomly19@github.com>
2019-11-08 23:31:58 +0100

    demande adhésion marche on peut accepter ou refuser

--- MediaNet_Staff/public/index.php
+++ MediaNet_Staff/public/index.php
@@ -64,11 +64,14 @@ $app->get('/documents', "\\app\\Controllers\\documentController:voir")->setName(
 $app->post('/documents', "\\app\\Controllers\\documentController:creerDocuements");
 
 
+$app->get('/ladhesion', "\\app\\Controllers\\adhesionController:adhesion")->setName('ladhesion');
+
 
 $app->get('/doc{id}', "\\app\\Controllers\\documentController:modifier")->setName('modifierdocuments');
 $app->post('/doc{id}', "\\app\\Controllers\\documentController:update");
 
-
+$app->get('/accepter{id}', "\\app\\Controllers\\adhesionController:accepter")->setName('accepter');
+$app->get('/refuser{id}', "\\app\\Controllers\\adhesionController:refuser")->setName('refuser');
 
 $app->get('/ajouterdoc', "\\app\\Controllers\\documentController:ajouter")->setName('ajouterdoc');
 $app->post('/ajouterdoc', "\\app\\Controllers\\documentController:afficher");
